e44e1a7443b8d4c92146338452fb05d614f3ca77
Suraj kushwaha <skk74143@gmail.com>
2022-06-30 12:56:19 +0530

    week 2 day 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { CovidData } from './componets/CovidData';
+import { CovidData } from './componets/Week1/CovidData';
 import { First } from './componets/Week2/First';
+import { Card, Container,Button } from 'react-bootstrap';
 
 function App() {
   return (
     <>
-    <div className="container">
+      <Container fluid>      
 
-    
-    <CovidData/>
-    {/* <First/> */}
-    </div>
+
+        {/* <CovidData /> */}
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top"  src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        {/* <First/> */}
+      </Container>
     </>
   );
 }
 
-function Add(){
-  var a =100,b=50;
-  return(
+function Add() {
+  var a = 100, b = 50;
+  return (
     <div>
-      <h1>{a+b}</h1>
+      <h1>{a + b}</h1>
     </div>
   )
 }
 
 export default App;
-export {Add};
+export { Add };
